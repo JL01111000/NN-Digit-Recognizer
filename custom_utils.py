0dc90f78a787795ce496971bcd9003d83c8b9205
JLaw <108596558+JL01111000@users.noreply.github.com>
2023-01-05 18:06:32 -0400

    Added main project files

---
+++ custom_utils.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+plt.style.use('./custom.mplstyle')
+
+def load_data():
+    X = np.load("data/X.npy")
+    y = np.load("data/y.npy")
+    return X, y
+
+def display_errors(model,X,y):
+    f = model.predict(X)
+    yhat = np.argmax(f, axis=1)
+    doo = yhat != y[:,0]
+    idxs = np.where(yhat != y[:,0])[0]
+    if len(idxs) == 0:
+        print("no errors found")
+    else:
+        cnt = min(8, len(idxs))
+        fig,ax = plt.subplots(1,cnt, figsize=(5,1.2))
+        fig.tight_layout(pad=0.13,rect=[0, 0.03, 1, 0.80])
+
+        for i in range(cnt):
+            j = idxs[i]
+            X_reshaped = X[j].reshape((20,20)).T
+
+            # Display the image
+            ax[i].imshow(X_reshaped, cmap='gray')
+
+            # Predict using the Neural Network
+            prediction = model.predict(X[j].reshape(1,400))
+            prediction_p = tf.nn.softmax(prediction)
+            yhat = np.argmax(prediction_p)
+
+            # Display the label above the image
+            ax[i].set_title(f"{y[j,0]},{yhat}",fontsize=10)
+            ax[i].set_axis_off()
+            fig.suptitle("Label, yhat", fontsize=12)
+    return(len(idxs))
